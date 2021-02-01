d35e4467ce232c4f52d3841f2b46667f5ad0c030
Jay <yimjay2@naver.com>
2021-02-01 14:52:06 +0900

    Qt Start

---
+++ viewer.h
@@ -0,0 +1,25 @@
+#ifndef VIEWER_H
+#define VIEWER_H
+
+#include <QWidget>
+#include <QDir>
+#include <QtMultimedia/QMediaPlayer>
+#include <QtMultimedia/QMediaPlaylist>
+#include <QtMultimediaWidgets/QVideoWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Viewer; }
+QT_END_NAMESPACE
+
+class Viewer : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Viewer(QWidget *parent = nullptr);
+    ~Viewer();
+
+private:
+    Ui::Viewer *ui;
+};
+#endif // VIEWER_H
