30c94d64c144ac5ff4e6cbd2185fa0dd340b5897
Jay <yimjay2@naver.com>
2021-02-09 15:02:03 +0900

    WindowsMediaFoundation (WIP)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 
 int main(int argc, char *argv[])
 {
+    qputenv("QT_MULTIMEDIA_PREFERRED_PLUGINS","windowsmediafoundation");
     QApplication a(argc, argv);
+    qputenv("QT_MULTIMEDIA_PREFERRED_PLUGINS","windowsmediafoundation");
     QCoreApplication::setApplicationName("Strabismus Viewer");
     Viewer w;
     //w.setFixedSize(1090,626);
